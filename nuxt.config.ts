a08737f2657d14091d77fb6ae7404ae10d123b1d
prismistim <info@snowsphere.net>
2022-09-04 23:31:43 +0900

    change: tailwindをnuxtのプラグインから読み込むように変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        }
-      }
-    }
-  },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
   css: [
-    '@/assets/css/index.css'
+    '@/assets/css/index.css',
   ]
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,6 @@ module.exports = {
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
   ],
   theme: {
     extend: {},
